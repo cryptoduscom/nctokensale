bf31d93577c08eb0a2b3c06fe07b27c7e58f2ba5
vbcca <vybccal@gmail.com>
2018-10-21 20:40:23 +0300

    initial commit

---
+++ contracts/Ico.sol
@@ -0,0 +1,58 @@
+pragma solidity 0.4.24;
+
+import "openzeppelin-solidity/contracts/lifecycle/Pausable.sol";
+
+contract Ico is Pausable {
+
+  bool public isFinalized = false;
+  bool public isStarted = false;
+
+  event Finalized();
+  event Started();
+  event Invested(address purchaser, address beneficiary, uint256 amount);
+
+  modifier whenStarted() {
+    require(isStarted);
+    _;
+  }
+
+  modifier whenNotFinalized() {
+    require(!isFinalized);
+    _;
+  }
+
+  constructor() public  {
+  }
+
+  function start() public onlyOwner {
+    require(!isStarted);
+    emit Started();
+    isStarted = true;
+  }
+
+  function finalize() public onlyOwner {
+    require(!isFinalized);
+    emit Finalized();
+    isFinalized = true;
+  }
+
+  function () external payable {
+    invest(msg.sender);
+  }
+
+  function invest(address _beneficiary)
+    public
+    whenStarted
+    whenNotPaused
+    whenNotFinalized
+    payable {
+
+    uint256 _weiAmount = msg.value;
+    require(_beneficiary != address(0));
+    require(_weiAmount != 0);
+
+    emit Invested(msg.sender, _beneficiary, _weiAmount);
+
+    owner.transfer(_weiAmount);
+  }
+}
---
+++ contracts/Token.sol
@@ -0,0 +1,44 @@
+pragma solidity 0.4.24;
+
+import "openzeppelin-solidity/contracts/token/ERC20/CappedToken.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/PausableToken.sol";
+
+contract Token is PausableToken {
+
+  uint256 private constant TOKEN_SUPPLY = 1 * 10**27;
+
+  string public constant name = "Nitecrawler";
+  string public constant symbol = "NCRL";
+  uint8 public constant decimals = 18;
+
+  event TransferingWithMessage(string message);
+
+  constructor() public  {
+    totalSupply_ = TOKEN_SUPPLY;
+    balances[msg.sender] = TOKEN_SUPPLY;
+  }
+
+  function transferWithMsg(
+    address _to,
+    uint256 _value,
+    string _msg
+  )
+    public whenNotPaused returns (bool)
+  {
+    emit TransferingWithMessage(_msg);
+    return super.transfer(_to, _value);
+  }
+
+  function transferFromWithMsg(
+    address _from,
+    address _to,
+    uint256 _value,
+    string _msg
+  )
+    public whenNotPaused returns (bool)
+  {
+    emit TransferingWithMessage(_msg);
+    return super.transferFrom(_from, _to, _value);
+  }
+
+}
